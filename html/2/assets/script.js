f0e5e158d19660d20fa91606121f6c1a18136976
masipah <36018369+masipah@users.noreply.github.com>
2023-08-18 05:16:51 -0700

    two

--- html/2/assets/script.js
+++ html/2/assets/script.js
@@ -2,7 +2,7 @@
 
 if ($(window).width() >= 480) {
 function changeImg(imgNumber)	{
-			var myImages = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg"]; 
+			var myImages = ["/bg1.jpg", "/bg2.jpg", "/bg3.jpg", "/bg4.jpg", "/bg5.jpg", "/bg6.jpg", "/bg7.jpg", "/bg9.jpg", "/bg10.jpg", "/bg11.jpg"]; 
 			var imgShown = document.body.style.backgroundImage;
 			var newImgNumber =Math.floor(Math.random()*myImages.length);
 			document.body.style.backgroundImage = 'url('+myImages[newImgNumber]+')';
